a5a11fb71b6fab96cd32e7c42a795d8a3503feef
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-02-08 09:49:26 -0600

    Configuración del proyecto

--- ViewModelLiveDataKotlin/app/src/main/java/page/shellcore/tech/android/viewmodellivedatakotlin/MainActivity.kt
+++ ViewModelLiveDataKotlin/app/src/main/java/page/shellcore/tech/android/viewmodellivedatakotlin/MainActivity.kt
@@ -1,12 +1,22 @@
 package page.shellcore.tech.android.viewmodellivedatakotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import page.shellcore.tech.android.viewmodellivedatakotlin.ui.ViewModelActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupView()
     }
+
+    private fun setupView() {
+        btnViewModel.setOnClickListener { startActivity(Intent(applicationContext, ViewModelActivity::class.java)) }
+    }
+
 }
---
+++ ViewModelLiveDataKotlin/app/src/main/java/page/shellcore/tech/android/viewmodellivedatakotlin/ui/ViewModelActivity.kt
@@ -0,0 +1,13 @@
+package page.shellcore.tech.android.viewmodellivedatakotlin.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import page.shellcore.tech.android.viewmodellivedatakotlin.R
+
+class ViewModelActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_model)
+    }
+}
