b867405cafadefbbfd3e4c5cbbfcff89a8f76974
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-02-09 15:53:32 -0600

    Creación de funciones para convertir un string separado por comas a lista de enteros y viceversa

---
+++ MVVMKotlin/app/src/main/java/page/shellcore/tech/android/mvvmkotlin/db/GithubTypeConverters.kt
@@ -0,0 +1,24 @@
+package page.shellcore.tech.android.mvvmkotlin.db
+
+import android.util.Log
+import androidx.room.TypeConverter
+
+object GithubTypeConverters {
+
+    @TypeConverter
+    @JvmStatic
+    fun stringToIntList(data: String?): List<Int>? = data?.let {string ->
+        string.split(",").mapNotNull {subString ->
+            try {
+                subString.toInt()
+            } catch (ex: NumberFormatException) {
+                Log.e("StringToIntList", "No puede convertir a número: ${ex.localizedMessage}")
+                null
+            }
+        }
+    }
+
+    @TypeConverter
+    @JvmStatic
+    fun intListToString(ints: List<Int>?): String? = ints?.joinToString { "," }
+}
