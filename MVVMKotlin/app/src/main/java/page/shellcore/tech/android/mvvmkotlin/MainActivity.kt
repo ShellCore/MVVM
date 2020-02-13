b2408e1f980ee7521490b66455c880ed9dbbe101
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-02-13 16:17:08 -0600

    Se implemento MainActivity

--- MVVMKotlin/app/src/main/java/page/shellcore/tech/android/mvvmkotlin/MainActivity.kt
+++ MVVMKotlin/app/src/main/java/page/shellcore/tech/android/mvvmkotlin/MainActivity.kt
@@ -1,12 +1,21 @@
 package page.shellcore.tech.android.mvvmkotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), HasAndroidInjector {
+
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun androidInjector(): AndroidInjector<Any> = dispatchingAndroidInjector
 }
